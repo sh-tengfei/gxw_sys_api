48c752740af4051a60e8b3cc3e9d650ba9141763
shangtengfei <shangtengfei@vipkid.com.cn>
2020-06-27 23:52:18 +0800

    fix

--- app/schedule/order-complete-timeout.js
+++ app/schedule/order-complete-timeout.js
@@ -5,7 +5,7 @@ class OrderCompleteTimeout extends Subscription {
   // 通过 schedule 属性来设置定时任务的执行间隔等配置
   static get schedule() {
     return {
-      cron: '0 0 2 * * ?', // 每日2点执行
+      cron: '0 0 1 * * ?', // 每日2点执行
 			type: 'all', // 指定所有的 worker 都需要执行
 			immediate: true,
     };
