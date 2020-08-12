5b24181298921e46b8c9603aabb81dd36045f6fb
shangtengfei <shangtengfei@vipkid.com.cn>
2020-08-12 16:35:59 +0800

    fix

---
+++ config/config.dev.js
@@ -0,0 +1,31 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+var defaultConfig = require('./config.default')
+
+defaultConfig.mongoose = {
+  url: 'mongodb://www.gxianwang.com:9001/gxianwangDev',
+  options: {
+    user: 'dev1',
+    pass: '!Qing001401',
+    config: { autoIndex: false },
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
+}
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  exports = defaultConfig
+
+  
+  return defaultConfig
+};
