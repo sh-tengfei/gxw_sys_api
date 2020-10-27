c21f0390f7e6f0a2457aae53bacb7d531b90b5e6
shangtengfei <shangtengfei@vipkid.com.cn>
2020-10-27 19:18:34 +0800

    fix

--- app/config/productType.js
+++ app/config/productType.js
@@ -9,24 +9,24 @@ export const productType = [
     id: 2,
     iconSrc: '//static.gxianwang.cn/fe-static/mall/o7r8bm5tm8c0'
   },
-  // {
-  //   label: '健康洗护',
-  //   id: 3,
-  //   iconSrc: '//static.gxianwang.cn/fe-static/mall/vrtqjat99lk0'
-  // },
-  // {
-  //   label: '美妆',
-  //   id: 4,
-  //   iconSrc: '//static.gxianwang.cn/fe-static/mall/t8js01dacqk0'
-  // },
-  // {
-  //   label: '干果',
-  //   id: 5,
-  //   iconSrc: '//static.gxianwang.cn/fe-static/mall/pcjte8ootoo0'
-  // },
-  // {
-  //   label: '外套',
-  //   id: 6,
-  //   iconSrc: '//static.gxianwang.cn/fe-static/mall/lvrlq7gcvlc0'
-  // }
+  {
+    label: '健康洗护',
+    id: 3,
+    iconSrc: '//static.gxianwang.cn/fe-static/mall/vrtqjat99lk0'
+  },
+  {
+    label: '美妆',
+    id: 4,
+    iconSrc: '//static.gxianwang.cn/fe-static/mall/t8js01dacqk0'
+  },
+  {
+    label: '干果',
+    id: 5,
+    iconSrc: '//static.gxianwang.cn/fe-static/mall/pcjte8ootoo0'
+  },
+  {
+    label: '外套',
+    id: 6,
+    iconSrc: '//static.gxianwang.cn/fe-static/mall/lvrlq7gcvlc0'
+  }
 ]
