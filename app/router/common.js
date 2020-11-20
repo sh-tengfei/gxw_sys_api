acdbd78944c6b456a32738b31d179b057c770a63
shangtengfei <shangtengfei@vipkid.com.cn>
2020-11-20 17:24:50 +0800

    fix

--- app/router/common.js
+++ app/router/common.js
@@ -3,5 +3,5 @@ export default function({ router, controller: contr, jwt }) {
   router.get('/admin/productType', jwt, contr.config.getProductType)
   router.get('/small/types', contr.config.getProductType)
 
-  router.put('/common/productType', contr.config.upProductType)
+  router.put('/common/productType', jwt, contr.config.upProductType)
 }
