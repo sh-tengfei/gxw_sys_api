6b548116e7e4ee8463f0dea3e59178da3a936986
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-18 17:18:28 +0800

    fix

--- db_backup.sh
+++ db_backup.sh
@@ -2,7 +2,7 @@
 
 now_mongodb="/data/backup/now_mongodb"
 
-mongodump --port 9001 -u "devAdmin" -p "!Qing001401" --gzip --authenticationDatabase "gxianwangDev" -o $now_mongodb
+mongodump --port 9001 -u "devAdmin" -p "!Qing001401" --gzip --authenticationDatabase "gxianwangDev" --db "gxianwangDev" -o $now_mongodb
 
 basepath="/data/backup/mongodb"
 if [ ! -d "$basepath" ]; then
---
+++ restore.sh
@@ -0,0 +1,1 @@
+mongorestore --host 127.0.0.1 --port 9001 -d gxianwangDev -u gxianwangAdmin --gzip --authenticationDatabase "admin" /Users/vipkid/www/private/gxianwangDev
