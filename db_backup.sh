5162579a69bbc3c355787d7ddd0ab16e5b27d578
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-18 15:56:02 +0800

    fix

--- db_backup.sh
+++ db_backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+now_mongodb="/data/backup/now_mongodb"
+
+mongodump --port 9001 -u "devAdmin" -p "!Qing001401" --gzip --authenticationDatabase "gxianwangDev" -o $now_mongodb
+
 basepath="/data/backup/mongodb/$(date +%Y%m%d)"
 if [ ! -d "$basepath" ]; then
   mkdir -p "$basepath"
 fi
 
-mongodump --port 9001 -u "devAdmin" -p "!Qing001401" --gzip --authenticationDatabase "gxianwangDev" -o $basepath
+zip -r "$basepath.zip" $now_mongodb
 
 find /data/backup/mongodb/ -mtime +30 -name "db*" -exec rm -rf {} \;
-
-zip -r "/data/backup/mongodb/$(date +%Y%m%d).zip" "/data/backup/mongodb/$(date +%Y%m%d)"
