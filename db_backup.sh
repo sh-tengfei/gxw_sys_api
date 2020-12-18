74186d9d0ff156ee3492e42d70caec5894f4ae39
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-18 15:46:15 +0800

    fxo

--- db_backup.sh
+++ db_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-basepath="/data/backup/mongodb/db$(date +%Y%m%d%H)"
+basepath="/data/backup/mongodb/$(date +%Y%m%d)"
 if [ ! -d "$basepath" ]; then
   mkdir -p "$basepath"
 fi
@@ -8,3 +8,5 @@ fi
 mongodump --port 9001 -u "devAdmin" -p "!Qing001401" --gzip --authenticationDatabase "gxianwangDev" -o $basepath
 
 find /data/backup/mongodb/ -mtime +30 -name "db*" -exec rm -rf {} \;
+
+zip -r "/data/backup/mongodb/$(date +%Y%m%d).zip" "/data/backup/mongodb/$(date +%Y%m%d)"
