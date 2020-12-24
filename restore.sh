eb6badb026cf95062566ef38723f5cc4715c3d94
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-24 19:42:58 +0800

    fix

--- restore.sh
+++ restore.sh
@@ -1,2 +1,2 @@
 mongorestore --host 127.0.0.1 --port 9001 -d guoxianDevelop -u guoxian_dev --gzip --authenticationDatabase "admin" /data/backup/mongodb/gxianwangDev #公司恢复命令
-#/usr/local/Cellar/mongodb-community@4.2/4.2.9/bin/mongorestore --host 127.0.0.1 --port 9001 -d gxianwangDev --gzip /data/backup/mongodb/data/backup/now_mongodb/gxianwangDev #个人本地恢复命令
+#/usr/local/Cellar/mongodb-community@4.2/4.2.9/bin/mongorestore --host 127.0.0.1 --port 9001 -d gxianwangDev --gzip /data/backup/mongodb/gxianwangDev #个人本地恢复命令
