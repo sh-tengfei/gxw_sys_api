268a6ea970716e65b7b7a3a1bfd97de8fcc5835f
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-25 13:05:12 +0800

    fix

--- restore.sh
+++ restore.sh
@@ -12,5 +12,5 @@ cd "$path"
 
 unzip "$path/$1" 
 
-mongorestore --host 127.0.0.1 --port 9001 -d guoxianDevelop -u restore --gzip --authenticationDatabase "admin" "$path/gxianwangDev" #!Qing001401
+mongorestore --host 127.0.0.1 --port 9001 -d "$2" -u restore --gzip --authenticationDatabase "admin" "$path/gxianwangDev" #!Qing001401
 
