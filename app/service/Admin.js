60656c94bff156f53f7c2c9f51ed2ccf2e1e37e4
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-25 15:36:03 +0800

    fix

--- app/service/Admin.js
+++ app/service/Admin.js
@@ -44,15 +44,20 @@ class AdminService extends Service {
   }
 
   async initialUser() {
+    let env = this.app.config.env
+    let onlinePwd = '!guoxianwang123'
+    if (env !== 'prod') {
+      onlineId = '123456'
+    }
     const users = await this.find()
     if (users.length === 0) {
       await this.create({
         username: 'root',
-        password: md5Pwd('123456')
+        password: md5Pwd(onlinePwd)
       })
       await this.create({
         username: 'admin',
-        password: md5Pwd('123456')
+        password: md5Pwd(onlinePwd)
       })
     }
   }
