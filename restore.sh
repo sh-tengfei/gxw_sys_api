c95fbca31685f40be6ee8a65b786bc1c24b9f9a0
shangtengfei <shangtengfei@vipkid.com.cn>
2020-12-25 11:26:14 +0800

    fix

---
+++ restore.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo $1
+
+path=~/data/mongodb
+
+if [ ! -d "$path" ]; then
+ mkdir -p "$path"
+fi
+
+cd "$path"
+
+unzip "$path/$1" 
+
+mongorestore --host 127.0.0.1 --port 9001 -d guoxianTest -u restore --gzip --authenticationDatabase "admin" "$path/gxianwangDev" #!Qing001401
+
--- synchro_ali.sh
+++ synchro_ali.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
+
 path=~/data/mongodb/
 if [ ! -d "$path" ]; then
   mkdir -p "$path"
 fi
 
 scp -r root@39.99.200.65:/data/backup/mongodb/* ~/data/mongodb/
-
-path=~/data/mongodb/gxianwangDev
-
-mongorestore --host 127.0.0.1 --port 9001 -d guoxianDevelop -u restore --gzip --authenticationDatabase "admin" "$path" #!Qing001401
 
