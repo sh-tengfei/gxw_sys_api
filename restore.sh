7f95e36131ebf444c68c5d625c17886c98e72906
shangtengfei <shangtengfei@vipkid.com.cn>
2021-02-01 14:14:16 +0800

    restore

--- restore.sh
+++ restore.sh
@@ -3,10 +3,20 @@
 echo $1
 echo $2
 # 参数$1是解压的带日期文件夹 $2应该是要恢复的数据库名称
-# 恢复数据库要情况
+# 恢复数据库要提前删除内容
+if [ ! "$1" ]; then
+  echo '目标解压文件不存在目标'
+  exit;
+fi
+
+if [ ! "$2" ]; then
+  echo '目标数据库参数不存在'
+  exit;
+fi
 
 mongodb='mongo 127.0.0.1:9001'
 $mongodb <<EOF
+
 use admin
 db.auth('root', '!Qing001401')
 use $2
@@ -22,8 +32,12 @@ if [ ! -d "$path" ]; then
 fi
 
 cd "$path"
+unzip "$path/$1" <<EOF
+A
+exit;
+EOF
 
-unzip "$path/$1" 
-
-mongorestore --host 127.0.0.1 --port 9001 -d "$2" -u restore --gzip --authenticationDatabase "admin" "$path/guoxianDevelop" #!Qing001401
+mongorestore --host 127.0.0.1 --port 9001 -d "$2" -u restore --gzip --authenticationDatabase "admin" "$path/guoxianDevelop" <<EOF 
+!Qing001401
+EOF
 
