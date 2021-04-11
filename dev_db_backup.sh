71d1abdf03df3cb51d0e1d269c033f3b7b6acee2
shangtengfei <sh_tengda@163.com>
2021-04-11 21:52:11 +0800

    fix

--- dev_db_backup.sh
+++ dev_db_backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-now_mongodb="/data/backup/now_mongodb"
+now_mongodb="/data/backup/now_dev_mongodb"
 
 mongodump --port 9001 -u "backup" -p "!Qing001401" --gzip --authenticationDatabase "admin" --db "guoxianDevelop" -o $now_mongodb
 
-basepath="/data/backup/mongodb"
+basepath="/data/backup/mongodb_dev"
 if [ ! -d "$basepath" ]; then
   mkdir -p "$basepath"
 fi
@@ -13,4 +13,4 @@ cd /data/backup/now_mongodb
 
 zip -r "$basepath/$(date +%Y%m%d).zip" guoxianDevelop
 
-find /data/backup/mongodb/ -mtime +30 -name "db*" -exec rm -rf {} \;
+find /data/backup/mongodb_dev/ -mtime +30 -name "db*" -exec rm -rf {} \;
