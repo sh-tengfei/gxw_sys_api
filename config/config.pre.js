b6dcb20332792758c6de58e4ff49ebca80757d94
shangtengfei <sh_tengda@163.com>
2021-07-04 23:13:07 +0800

    fix

--- config/config.pre.js
+++ config/config.pre.js
@@ -5,37 +5,38 @@
 /**
  * @param {Egg.EggAppInfo} appInfo app info
  */
-module.exports = appInfo => {
-  return {
-    jwt: {
-      secret: 'guoxianwang',
-    },
-    cluster: {
-      listen: {
-        port: 8102,
-        hostname: '127.0.0.1',
-      }
-    },
-    logger: {
-      dir: '/home/logs/api/pre',
-    },
-    mongoose: {
-      url: 'mongodb://127.0.0.1:9001/guoxianwangPre',
-      options: {
-        user: 'guoxianwangPre',
-        pass: '!Qing001401',
-        config: { autoIndex: false },
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-      }
-    },
-    session: {
-      key: 'gxw', // 设置 Session cookies 里面的 key
-      maxAge: 7 * 24 * 3600 * 1000, // 7 天
-      httpOnly: true,
-      encrypt: true,
-      renew: true, // 每次刷新页面，Session 都会被延期。
+module.exports = {
+  jwt: {
+    secret: 'guoxianwang',
+  },
+  cluster: {
+    listen: {
+      port: 8102,
+      hostname: '127.0.0.1',
     }
+  },
+  logger: {
+    dir: '/home/logs/api/pre',
+  },
+  mongoose: {
+    url: 'mongodb://127.0.0.1:9001/guoxianwangPre',
+    options: {
+      user: 'guoxianwangPre',
+      pass: '!Qing001401',
+      config: { autoIndex: false },
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  },
+  session: {
+    key: 'gxw', // 设置 Session cookies 里面的 key
+    maxAge: 7 * 24 * 3600 * 1000, // 7 天
+    httpOnly: true,
+    encrypt: true,
+    renew: true, // 每次刷新页面，Session 都会被延期。
+  },
+  wxPayment: {
+    wxurl: 'https://mall.gxianwang.com/api/small/wxPayNotify', // 微信小程序回调
   }
 }
